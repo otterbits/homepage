29dde10def376290079fcedb8d31675ffaed3b1c
uoohoon <hoonixox@naver.com>
2023-11-16 14:46:48 +0900

    예시 삭제 및 otterbit 주, 부제 생성

--- streamlit_page.py
+++ streamlit_page.py
@@ -1,11 +1,5 @@
 import streamlit as st
-st.write('# Youtube view')
-st.write('## view')
-view = [100,150,30]
-view
-st.write('## bar chart')
-st.bar_chart(view)
 
-import pandas as pd
-sview = pd.Series(view)
-sview
+
+st.title('Welcome to :rainbow[Otterbit] world')
+st.write('## coding is very :red[_awesome_] :sunglasses:')
