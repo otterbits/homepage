f9e8c9494e70b5b649014e10af3e6aa23b74afb3
uoohoon <hoonixox@naver.com>
2023-11-16 14:47:26 +0900

    구분선 및 유튜브 링크 버튼 생성

--- streamlit_page.py
+++ streamlit_page.py
@@ -3,3 +3,6 @@ import streamlit as st
 
 st.title('Welcome to :rainbow[Otterbit] world')
 st.write('## coding is very :red[_awesome_] :sunglasses:')
+
+st.divider()
+st.link_button("youtube", "https://www.youtube.com/@racoonByteotterBit/")
