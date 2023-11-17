f2a487e1dde0b6515d4e78a43e15736f42add441
otter <144116866+otterbits@users.noreply.github.com>
2023-11-17 19:07:00 +0900

    영상 추가, 폴더 삭제, 배경색상 변경 (#1)
    * 각 tab에 2개의  columns 추가
    
    * 각 col에 영상 추가
    
    * 각각 col에 각 영상이 들어가도록 수정
    
    * 제목이 출력되지 않는 오류 수정
    
    * flask 폴더 삭제 및 streamlit theme 폴더 생성
    
    * 배경색상 변경

--- streamlit_page.py
+++ streamlit_page.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import numpy as np
+
+
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: green;'>Otterbit world</h1>", unsafe_allow_html=True)
 st.write('## coding is very :red[_awesome_] :sunglasses:')
@@ -8,15 +10,22 @@ st.divider()
 st.link_button("youtube", "https://www.youtube.com/@racoonByteotterBit/")
 tab1, tab2, tab3= st.tabs(["INTRO", "PROJECT", "STUDY"])
 
-
 with tab1:
    st.write("### hello otter!")
-   st.video('https://youtu.be/u52CGaPWk1M?feature=shared', format='video/mp4')
+   col1, col2 = st.columns(2)
+   with col1:
+      st.video('https://youtu.be/u52CGaPWk1M?feature=shared', format='video/mp4')
 
 with tab2:
    st.write("### working hard with communication")
-   st.video('https://youtu.be/trS7V4g_8QI?feature=shared', format='video/mp4')
+   col1, col2 = st.columns(2)
+   with col1:
+      st.video('https://youtu.be/trS7V4g_8QI?feature=shared', format='video/mp4')
+   with col2:
+      st.video('https://youtu.be/NlOsPCwWegk?feature=shared', format='video/mp4')
 
 with tab3:
    st.write("### study knowledge")
-   st.video('https://youtu.be/_TeeNkU1fpY?feature=shared', format='video/mp4')
+   col1, col2 = st.columns(2)
+   with col1:
+      st.video('https://youtu.be/_TeeNkU1fpY?feature=shared', format='video/mp4')
