31651bf78d406eaaf7e80a5ff606f026956d28c4
uoohoon <hoonixox@naver.com>
2023-11-17 18:25:41 +0900

    project, study탭에 해당 영상 추가

--- streamlit_page.py
+++ streamlit_page.py
@@ -6,15 +6,16 @@ st.write('## coding is very :red[_awesome_] :sunglasses:')
 
 st.divider()
 st.link_button("youtube", "https://www.youtube.com/@racoonByteotterBit/")
-tab1, tab2, tab3 = st.tabs(["INTRO", "PROJECT", "STUDY"])
-
+tab1, tab2, tab3, tab4 = st.tabs(["INTRO", "PROJECT", "STUDY",])
 
 with tab1:
-   st.header("hello otter!")
+   st.write("### hello otter!")
    st.video('https://youtu.be/u52CGaPWk1M?feature=shared', format='video/mp4')
 
 with tab2:
-   st.header("working hard with communication")
+   st.write("### working hard with communication")
+   st.video('https://youtu.be/trS7V4g_8QI?feature=shared', format='video/mp4')
 
 with tab3:
-   st.header("study knowledge")
+   st.write("### study knowledge")
+   st.video('https://youtu.be/_TeeNkU1fpY?feature=shared', format='video/mp4')
