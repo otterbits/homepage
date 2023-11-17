a43ab5f731f60dd798559960264b1e6d1e524fd1
uoohoon <hoonixox@naver.com>
2023-11-17 18:17:37 +0900

    tab란 추가

--- streamlit_page.py
+++ streamlit_page.py
@@ -1,8 +1,9 @@
 import streamlit as st
-
+import numpy as np
 
 st.title('Welcome to :rainbow[Otterbit] world')
 st.write('## coding is very :red[_awesome_] :sunglasses:')
 
 st.divider()
 st.link_button("youtube", "https://www.youtube.com/@racoonByteotterBit/")
+tab1, tab2, tab3 = st.tabs(["INTRO", "PROJECT", "STUDY"])
